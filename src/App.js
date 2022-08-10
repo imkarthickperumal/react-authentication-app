667a404bc67eb075f2b5b8db3c41adc68f72b3b8
imkarthi16 <karthiveera@gmail.com>
2022-08-10 16:33:32 +0530

    project files setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { Switch, Route } from 'react-router-dom';
 
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-     <h1>Hello Karthick</h1>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/auth'>
+          <AuthPage />
+        </Route>
+        <Route path='/profile'>
+          <UserProfile />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
